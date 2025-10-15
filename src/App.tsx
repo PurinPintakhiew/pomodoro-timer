10b9ed50841ea18a9cb187a5df36c18de9a1cddc
Purin.P <dev.purin@gmail.com>
2025-10-15 23:21:27 +0700

    feat: tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultTime: number = 24 * 1000 * 60;
+
+  const [start, setStart] = useState(false);
+  const [time, setTime] = useState(defaultTime);
+
+  const timeFormat = (time: number) => {
+    const minute = time || "00";
+    const second = time || "00";
+    return `${minute}:${second}`;
+  }
 
   return (
     <>
-      <h1>Pomodoro Timer</h1>
+      <div className="bg-[#F56247] h-screen w-screen flex flex-col justify-center items-center">
+        <h1 className='text-5xl font-bold text-white'>Pomodoro Timer</h1>
+        <div className="rounded-md mt-4 p-8 bg-[#FA8E7A]">
+          <h1 className='text-5xl font-bold text-white'>{timeFormat(time)}</h1>
+          <div className="flex justify-center gap-2 mt-5">
+            {
+              start ?
+                <button
+                  className='rounded-2xl py-2 px-4 text-[#F56247] bg-[#FFEDD4] font-bold'
+                >
+                  Stop
+                </button>
+                :
+                <button
+                  className='rounded-2xl py-2 px-4 text-[#F56247] bg-[#FFEDD4] font-bold'
+                >
+                  Start
+                </button>
+            }
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(), // ✅ Tailwind goes here
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [
+          ['babel-plugin-react-compiler'], // ✅ only Babel plugins here
+        ],
       },
     }),
   ],
